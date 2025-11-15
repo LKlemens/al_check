cc5c0174680dade6feecd35cd88a8600b72f7a3d
LKlemens <klemens.lukaszczyk@gmail.com>
2025-11-15 15:19:03 +0100

    new project

---
+++ lib/al_check.ex
@@ -0,0 +1,18 @@
+defmodule AlCheck do
+  @moduledoc """
+  Documentation for `AlCheck`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AlCheck.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/al_check_test.exs
@@ -0,0 +1,8 @@
+defmodule AlCheckTest do
+  use ExUnit.Case
+  doctest AlCheck
+
+  test "greets the world" do
+    assert AlCheck.hello() == :world
+  end
+end
