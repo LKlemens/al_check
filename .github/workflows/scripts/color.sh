c084cf8da51464d6baf9486ac89af6d4c07f0763
LKlemens <klemens.lukaszczyk@gmail.com>
2026-06-14 08:31:15 +0200

    fix color.sh

--- .github/workflows/scripts/color.sh
+++ .github/workflows/scripts/color.sh
@@ -9,16 +9,17 @@ fi
 coverage=$(echo $1 | cut -d'.' -f-1)
 
 
+# badge-action prepends '#', so emit hex (not named colors) to get valid #RRGGBB
 if [[ $coverage -gt 90 ]]; then
-    color="brightgreen"
+    color="4c1"
 elif [[ $coverage -gt 80 ]]; then
-    color="green"
+    color="97ca00"
 elif [[ $coverage -gt 60 ]]; then
-    color="yellowgreen"
+    color="a4a61d"
 elif [[ $coverage -gt 40 ]]; then
-    color="yellow"
+    color="dfb317"
 else
-    color="red"
+    color="e05d44"
 fi
 
 echo $color
